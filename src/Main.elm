da01491022933228a2702060be0b6f4a86e0dfd5
amatgil <amatgilvinyes@gmail.com>
2025-12-21 01:05:04 +0100

    Funny snapshot

--- src/Main.elm
+++ src/Main.elm
@@ -21,19 +21,25 @@ update msg model =
 
 view : Model -> Html Msg
 view (Model day) =
-  div [] [ div [ style "display" "grid"
+  div [] [ h1 [] [ text "Holi :3" ]
+         , div [ style "display" "grid"
                , style "grid-template-columns" "1fr 3fr"
                , style "color" "#cdd6f4"]
             [ div [ bg "#1e1e2e"
-                 , fullHeight
-                 ] daysList
-            , div [ bg "#313244" ] [ text ("Dia és: " ++ String.fromInt day ) ] ]
-         ]
+                  , fullHeight ]
+                [div [ style "display" "flex"
+                     , style "flex-direction" "column"] daysList ]
+            , div [ bg "#313244" ] [ text ("Dia és: " ++ String.fromInt day ) ] ] ]
 
 daysList : List (Html Msg)
-daysList = List.map (\n -> button [onClick (ChangeDay n)]
+daysList = List.map (\n -> button [ onClick (ChangeDay n)
+                                  , style "text-align" "left"
+                                  , style "border" "none"
+                                  , bg "#1e1e2e"
+                                  , style "color" "#cdd6f4"]
                          [text ("Dia " ++ String.fromInt n)])
            (range 1 12)
 
 fullHeight = style "height" "100vh"
 bg         = style "background-color"
+
