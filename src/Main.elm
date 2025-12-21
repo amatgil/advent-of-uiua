be8eba582e37f39ea3100c24a01281cf22692f27
amatgil <amatgilvinyes@gmail.com>
2025-12-21 03:04:52 +0100

    It's all coming together

--- src/Main.elm
+++ src/Main.elm
@@ -8,14 +8,14 @@ import List exposing (..)
 import Catppuccin
 
 main =
-  Browser.document { init = Model 1, update = update, view = view }
+  Browser.sandbox { init = Model 1, update = update, view = view }
 
 type Msg = ChangeDay Int
 
 type Model = Model Int
 
 update : Msg -> Model -> Model
-update msg model =
+update msg _ =
   case msg of
     ChangeDay to -> Model to
 
@@ -24,7 +24,7 @@ view : Model -> Html Msg
 view (Model day) =
   div [] [ h1 [style "height" "1em"] [ text "Holi :3" ]
          , div [ style "display" "grid"
-               , style "grid-template-columns" "1fr 3fr"
+               , style "grid-template-columns" "1fr 4fr"
                , style "color" Catppuccin.text]
             [ div [ bg Catppuccin.base , fullHeight ]
                 [div [ style "display" "flex"
@@ -35,8 +35,12 @@ daysList : List (Html Msg)
 daysList = List.map (\n -> button [ onClick (ChangeDay n)
                                   , style "text-align" "left"
                                   , style "border" "none"
-                                  , bg Catppuccin.base
-                                  , style "color" Catppuccin.text]
+                                  , style "background-color" Catppuccin.base
+                                  , style "color" Catppuccin.text
+                                  , style "font-size" "1.5em"
+                                  , style "padding-top" "0.3em"
+                                  , style "padding-bottom" "0.3em"
+                                  , class "day-button"]
                          [text ("Dia " ++ String.fromInt n)])
            (range 1 12)
 
