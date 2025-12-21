31832596c277b4fc6b881d40b5ba47b1774f727b
amatgil <amatgilvinyes@gmail.com>
2025-12-21 01:50:38 +0100

    hmm

--- src/Main.elm
+++ src/Main.elm
@@ -5,9 +5,10 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import List exposing (..)
+import Catppuccin
 
 main =
-  Browser.sandbox { init = Model 1, update = update, view = view }
+  Browser.document { init = Model 1, update = update, view = view }
 
 type Msg = ChangeDay Int
 
@@ -21,25 +22,23 @@ update msg model =
 
 view : Model -> Html Msg
 view (Model day) =
-  div [] [ h1 [] [ text "Holi :3" ]
+  div [] [ h1 [style "height" "1em"] [ text "Holi :3" ]
          , div [ style "display" "grid"
                , style "grid-template-columns" "1fr 3fr"
-               , style "color" "#cdd6f4"]
-            [ div [ bg "#1e1e2e"
-                  , fullHeight ]
+               , style "color" Catppuccin.text]
+            [ div [ bg Catppuccin.base , fullHeight ]
                 [div [ style "display" "flex"
                      , style "flex-direction" "column"] daysList ]
-            , div [ bg "#313244" ] [ text ("Dia és: " ++ String.fromInt day ) ] ] ]
+            , div [ bg Catppuccin.surface0 ] [ text ("Dia és: " ++ String.fromInt day ) ] ] ]
 
 daysList : List (Html Msg)
 daysList = List.map (\n -> button [ onClick (ChangeDay n)
                                   , style "text-align" "left"
                                   , style "border" "none"
-                                  , bg "#1e1e2e"
-                                  , style "color" "#cdd6f4"]
+                                  , bg Catppuccin.base
+                                  , style "color" Catppuccin.text]
                          [text ("Dia " ++ String.fromInt n)])
            (range 1 12)
 
 fullHeight = style "height" "100vh"
 bg         = style "background-color"
-
